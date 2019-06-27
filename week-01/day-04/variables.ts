2c32303eb3aecb84f5999ad16a71bed7fdfb1bdd
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-27 10:22:53 +0200

    variables.ts added

---
+++ week-01/day-04/variables.ts
@@ -0,0 +1,14 @@
+const pi: number = 3.14;
+
+let times: number = 10;
+let myName: string = "Zsolt";
+
+
+   
+console.log(times);
+console.log(myName);
+
+
+let itCanBeUndefined: number;
+console.log(itCanBeUndefined);
+console.log(myName + 1);
