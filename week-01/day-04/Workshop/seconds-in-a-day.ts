94490a67ecdecd436294bbcb26cc28cd60dab198
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-27 14:52:53 +0200

    seconds in a day

---
+++ week-01/day-04/Workshop/seconds-in-a-day.ts
@@ -0,0 +1,6 @@
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+let secondsOfDay: number=0;
+secondsOfDay=24*60*60
+console.log(secondsOfDay-(currentHours*60*60+currentMinutes*60+currentSeconds));
