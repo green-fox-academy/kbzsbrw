7785170a0fa7f1f757b701e70f942ead8f09a917
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-27 12:24:48 +0200

    hello others

---
+++ week-01/day-04/Workshop/hello-others.ts
@@ -0,0 +1,6 @@
+let hello: string = "Hello, ";
+let exclamation: string = "!";
+
+console.log(hello+"Sanyi"+exclamation);
+console.log(hello+"Jenat"+exclamation);
+console.log(hello+"Zoli"+exclamation);
