ff42e073e956278307531f605608b126c7e039a5
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-27 14:18:47 +0200

    define basics

---
+++ week-01/day-04/Workshop/define-basic-info.ts
@@ -0,0 +1,7 @@
+let myName: string = "Zsolt Kosztolanyi-Baji";
+let age: number = 28;
+let height: number = 1.84;
+let married: boolean = false;
+
+
+console.log(myName+"\n"+age+"\n"+height+"\n"+married);
