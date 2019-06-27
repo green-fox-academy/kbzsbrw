a55a7de6278a953e21cf9f754404b5eedf4f4022
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-27 17:45:43 +0200

    today finished until the diamond

---
+++ week-01/day-04/Workshop/draw-diamond.ts
@@ -0,0 +1,21 @@
+//that's not working. yet
+
+/*let lineCount: number=7;
+let a: number=1;
+let empty: string="";
+let i=Math.floor(lineCount/2);
+let before=i;
+let after=i;
+let star: string="*";
+while (before<=i) {
+    console.log(empty);
+    star += "*";
+    before--;
+    after--;
+    a++;
+}
+*/
+let a: string="   *   ";
+console.log(a);
+a -= "*";
+console.log(a);
