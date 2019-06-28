297ee4ebfc169d8b8e3ca276f30640d5fd2478c9
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-28 14:07:07 +0200

    diamond was not perfect

--- week-01/day-04/Workshop/draw-diamond.ts
+++ week-01/day-04/Workshop/draw-diamond.ts
@@ -1,39 +1,50 @@
-let lineCount: number = 7;
-let half=Math.floor(7/2)+1;
+let lineCount: number = 12;
+let half=Math.floor(lineCount/2)+1;
 let space: string="";
 let star: string="*";
-for (let k: number = 0; k<half; k++) {
-    for (let j: number = 0; j<=half-k;j++) {
-    space+=" ";
+if (lineCount % 2 === 0) {
+    for (let k: number = 0; k<half; k++) {
+        for (let j: number = 0; j<=half-k;j++) {
+        space+=" ";
+        }
+        console.log(space+star);
+        star+="*";
+        star+="*";
+        space="";
     }
-    console.log(space+star);
-    star+="*";
-    star+="*";
-    space="";
-}
 
 
-space="   ";
-star="";
-for (let m: number = 0; m<half; m++) {
-    for (let n: number = 0; n<(lineCount)-2-(m*2);n++) {
-    star+="*";
+    space="   ";
+    star="*";
+    for (let m: number = 0; m<half; m++) {
+        for (let n: number = 0; n<(lineCount)-1-(m*2);n++) {
+        star+="*";
+        }
+        console.log(space+star);
+        space+=" ";
+        star="";
     }
-    console.log(space+star);
-    space+=" ";
+
+} else {
+    for (let k: number = 0; k<half; k++) {
+        for (let j: number = 0; j<=half-k;j++) {
+        space+=" ";
+        }
+        console.log(space+star);
+        star+="*";
+        star+="*";
+        space="";
+    }
+
+
+    space="   ";
     star="";
-}
-
-
-
-/*  for (let m: number = 0; m<lineCount; m++) {
-    for (let n: number = 0; n<=lineCount-m;n++) {
-    space+="";
+    for (let m: number = 0; m<half; m++) {
+        for (let n: number = 0; n<(lineCount)-2-(m*2);n++) {
+        star+="*";
+        }
+        console.log(space+star);
+        space+=" ";
+        star="";
     }
-    console.log(space+star);
-    star+=" ";
-    star+=" ";
-    space="";
 }
- */
-
