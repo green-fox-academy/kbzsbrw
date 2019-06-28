409b9b1ea4030bcd997ada825590926d32edf65d
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-06-28 10:32:10 +0200

    made the fucking pyramid

---
+++ week-01/day-04/Workshop/drar-pyramid.ts
@@ -0,0 +1,26 @@
+let lineCount: number = 7;
+/*let star: string = "*"
+//let half=Math.floor(lineCount/2);
+let before: string="a"
+for (let i: number=1; i<lineCount-1;i++) {
+    before+="a";
+}
+for (let k: number = 1; k<lineCount; k++) {
+    console.log(before+star+after);
+    before-="a";
+    after-="a";
+    star+="*";
+
+}*/
+let space: string="";
+let star: string="*";
+while (lineCount>=1) {
+    for (let j=lineCount-1; j>=1; j--){
+    space+="a";
+}
+    console.log(space+star);
+    star+="*";
+    star+="*";
+    space="";
+    lineCount--;
+}
--- week-01/day-04/Workshop/draw-diamond.ts
+++ week-01/day-04/Workshop/draw-diamond.ts
@@ -1,21 +1,14 @@
-//that's not working. yet
-
-/*let lineCount: number=7;
-let a: number=1;
-let empty: string="";
-let i=Math.floor(lineCount/2);
-let before=i;
-let after=i;
-let star: string="*";
-while (before<=i) {
-    console.log(empty);
-    star += "*";
-    before--;
-    after--;
-    a++;
-}
-*/
-let a: string="   *   ";
-console.log(a);
-a -= "*";
-console.log(a);
+let lineCount: number = 2;
+let doubleit = lineCount*2;
+    for(let i: number=0; i<doubleit; i++){
+        if (i < lineCount){
+            for(let j: number =0; j<i; j++){
+                console.log('*');
+            }
+        }
+        else if (i >= lineCount){
+            for(let j=doubleit; j>i; j--){
+                console.log('*');
+            }
+        }
+    }
