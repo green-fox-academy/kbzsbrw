cc38bf0f07ec5f9b694c727b78918cd7d96eb023
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-01 13:02:15 +0200

    before lunch

--- week-02/day-01/compare-length.ts
+++ week-02/day-01/compare-length.ts
@@ -2,6 +2,6 @@
 
 let firstList: number[] = [1,2,3];
 let secondList: number[] = [4,5,6,7];
-if (secondList.length>firstList.length) {
+if (secondList.length > firstList.length) {
     console.log("p2 is longer");
 }
