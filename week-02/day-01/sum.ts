7d9e88ad6bb6a07ee49068932c6678407645fb3d
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-01 11:34:53 +0200

    before the 2nd break

--- week-02/day-01/sum.ts
+++ week-02/day-01/sum.ts
@@ -26,3 +26,29 @@ let diffArray: [string, boolean, string] = ["Mark", true, "mazli"];
 console.log(diffArray);
 
 let onlyTwoArray: Array<boolean | number> = [2, false, 3, true, true, 3, 313];
+
+
+diffArray.splice(0, 1, "Zoli", );
+
+diffArray.forEach(function (item) {
+    console.log(item);
+})
+
+let doubled: number[] = [1,2,3,4,5].map(function(i){
+    return i *2;
+}).filter(function(item){
+    return item % 2 ===1;
+})
+console.log("doubled")
+
+console.log(doubled)
+let odds: number[] = [1,2,3,4,5].filter(function(item) {
+    return item % 2 !== 0;
+})
+console.log(odds);
+
+let array1: number[] = [1,2];
+let array2: number[] = [3,4];
+
+let array3: number[] = [...array1, ...array2];
+console.log(array3);
