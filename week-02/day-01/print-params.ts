24a7b8bd2ef3132ea0d0921eefaa5913716298a3
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-01 16:57:22 +0200

    print params

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+function printParams(input, input1, input2) {
+    console.log(input);
+    console.log(input1);
+    console.log(input2);
+    
+}
+printParams("mami", "papi", "aélksdjf");
