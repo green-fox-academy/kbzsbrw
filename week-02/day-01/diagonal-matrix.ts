2a1c5497c71b2ea1047a57c19b9c987f7443e3dc
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-01 15:06:47 +0200

    diagonal

---
+++ week-02/day-01/diagonal-matrix.ts
@@ -0,0 +1,9 @@
+"use strict";
+let rowNumber: number = 4;
+let row: number[] = [0, 0, 0, 0];
+
+for (let i=row.length; i>0; i--){
+    row[i-1]++;
+    console.log(row);
+    row[i-1]=0;
+}
---
+++ week-02/day-01/sum-all.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let ai: number[] = [3, 4, 5, 6, 7];
+let sum: number = 0;
+ai.forEach (function(nums) {
+    sum+=nums;
+})
+console.log(sum);
