c14f145947da7c967fe1d7c5633f8d5d367719fb
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-01 15:21:18 +0200

    reverse with for loop

--- week-02/day-01/reverse-list.ts
+++ week-02/day-01/reverse-list.ts
@@ -3,4 +3,10 @@
 let numList: number [] = [3, 4, 5, 6, 7];
 
 let sortedList: number[] = numList.sort((a,b)=>b-a);
-console.log(sortedList);
+console.log("v1: "+sortedList);
+
+let tempList: number[] = [];
+for (let i: number = 0; i<numList.length; i++) {
+    tempList[i] = numList[numList.length-1-i]
+}
+console.log("v2: "+tempList);
