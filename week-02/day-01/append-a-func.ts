8055ad89a35ae47913fbefd1b6c00e6c692c558a
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-01 15:42:41 +0200

    append a func

---
+++ week-02/day-01/append-a-func.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+let typo: string = "Chinchill";
+function appendAFunc(stringNeedA) {
+    stringNeedA=stringNeedA+"a";
+    console.log(stringNeedA);
+}
+appendAFunc(typo);
+appendAFunc("mazsol");
