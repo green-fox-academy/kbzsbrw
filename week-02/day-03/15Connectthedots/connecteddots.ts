d0c9d1109e962b5f4a620e6240d1e0265f5cd3f9
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-04 15:30:57 +0200

    just made a fox

--- week-02/day-03/15Connectthedots/connecteddots.ts
+++ week-02/day-03/15Connectthedots/connecteddots.ts
@@ -12,7 +12,17 @@ const ctx = canvas.getContext('2d');
 // Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
 // [120, 100], [85, 130], [50, 100]]
 
-let points: number [[]] = [[10, 10], [290,  10], [290, 290], [10, 290]];
-function connectDots(x) {
+let points: number [][] = [[10, 10], [290,  10], [290, 290], [10, 290]];
+let points2: number [][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],[120, 100], [85, 130], [50, 100]];
+
+function connectDots(parameter: number[][]) {
+    for (let i: number = 0; i<parameter.length+1; i++) {
+        ctx.beginPath();
+        ctx.moveTo(parameter[i][0],parameter[i][1]);
+        ctx.lineTo(parameter[i+1][0],parameter[i+1][1]);
+        ctx.stroke();
+    }
 
 }
+//connectDots(points);
+connectDots(points2);
