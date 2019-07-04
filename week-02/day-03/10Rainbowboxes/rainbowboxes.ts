cefff765fb08a9999c7e16524802a47d24b4ccb6
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-04 12:54:22 +0200

    example

--- week-02/day-03/10Rainbowboxes/rainbowboxes.ts
+++ week-02/day-03/10Rainbowboxes/rainbowboxes.ts
@@ -1,9 +1,9 @@
 'use strict';
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
 const myHeight = 400;
 const myWidth = 600;
 
-const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
@@ -14,11 +14,27 @@ const ctx = canvas.getContext('2d');
 let colors: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 function coloredCube (parameter: number) {
     let x: number = Math.floor(myHeight/parameter)*7+1;
-   for (let j: number = 0; j<Math.floor(myHeight/x)+1; j++) {
+    for (let j: number = 0; j<Math.floor(myHeight/x)+1; j++) {
         for (let i: number = 0; i<Math.floor(myWidth/x)+1; i++) {  
             ctx.fillStyle= colors[j];
             ctx.fillRect((x*i),x*j,x,x);
-            }
-   }    
+        }
+    }    
 }
 coloredCube(50);
+
+/*that's from somebody else to try
+const canvasWidth = 600;
+const canvasHeigth = 400;
+
+function calcColor(min, max, val) {
+   let minHue = 240, maxHue = 0;
+   let curPercent = (val - min) / (max - min);
+   let color = "hsl(" + ((curPercent * (maxHue - minHue)) + minHue) + ", 100%, 50%)";
+   return color;
+}
+
+for (var i = 400; i > 0; i -= 1) {
+   ctx.fillStyle = calcColor(240, 0, i);
+   ctx.fillRect(canvasWidth / 2 - i / 2, canvasHeigth / 2 - i / 2, i, i);
+}*/
