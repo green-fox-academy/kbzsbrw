fcb52a03ea72ffe342c358d3c9faf02b4d36295b
kbzsbrw <kosztolanyi.baji.zsolt@gmail.com>
2019-07-09 13:03:34 +0200

    count lines with the proper return value

--- week-03/day-02/02count-lines.ts
+++ week-03/day-02/02count-lines.ts
@@ -3,20 +3,18 @@
 // It should return zero if it can't open the file, and
 // should not raise any error.
 
-'use strict';
+"use strict";
 
 declare function require(path: string): any;
 const fs = require("fs");
 
-function filenameAsString(filename: string): string {
-    try {
-        let lines: string[]= fs.readFileSync(filename, 'utf8').split('\n');
-        console.log(lines.length);
-        //console.log(fs.readFileSync(filename, 'utf8').length);
-    } catch (e) {
-        console.log(0);
-    }
-
-    return ;
+function filenameAsString(filename: string): number {
+  try {
+    let lines: string[] = fs.readFileSync(filename, "utf8").split("\n");
+    return lines.length;
+    //console.log(fs.readFileSync(filename, 'utf8').length);
+  } catch (e) {
+    console.log(0);
+  }
 }
-filenameAsString('my-files.txt');
+console.log(filenameAsString("my-files.txt"));
