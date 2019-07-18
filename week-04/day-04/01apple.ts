35e18070414a395941defaf77bd2acc2e5c930f9
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-07-18 16:19:47 +0200

    started with apple

---
+++ week-04/day-04/01apple.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let test = require('tape');
+
+class Apple {
+
+    getApple (): string {
+        return "apple";
+    }
+}
+
+test('check if apple',t=>{
+    let apple1: Apple = new Apple();
+    t.equal(apple1.getApple(), 'apple');
+}
+
+);
--- week-04/day-04/matrev/test.ts
+++ week-04/day-04/matrev/test.ts
@@ -2,7 +2,7 @@
 
 import { square } from './matrev';
 let test = require('tape');
-
+console.log("wtf");
 test(t => {
 t.equals(square(3), 9);
 t.end();
