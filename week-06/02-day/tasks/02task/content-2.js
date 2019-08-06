1802c1c12e8b0c36d775a6baf958fda9c92472ec
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-08-06 15:15:11 +0200

    content2

--- week-06/02-day/tasks/02task/content-2.js
+++ week-06/02-day/tasks/02task/content-2.js
@@ -6,3 +6,8 @@ console.log(liNodelist);
 for (let i =liNodelist.length-1; i>=0; i--) {
 liNodelist[i].innerHTML=newContent[i]
 }
+
+let ulTag = document.querySelector("ul");
+console.log(ulTag);
+ulTag.setAttribute("style", "background-color: red" )
+console.log(ulTag);
