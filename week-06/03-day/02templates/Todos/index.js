cec541ecf643916660b78e53fabfbbe835569bb2
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-08-07 14:36:47 +0200

    todos ready

---
+++ week-06/03-day/02templates/Todos/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const todos = ["get up", "survive", "go back to bed"];
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("home", { todos: todos });
+});
+
+app.listen(PORT, () => console.log(`I am listening on port ${PORT}`));
