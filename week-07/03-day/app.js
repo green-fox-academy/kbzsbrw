db4038d621dd329d1940ffe6e3952e7eda4344f2
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-08-14 12:55:22 +0200

    basic int&doubling

---
+++ week-07/03-day/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const app=express();
+const PORT = 8080;
+const path = require('path');
+app.use(express.static("assets"));
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/doubling', (req,res) => {
+    let response = {};
+    if(req.query.input!==undefined){
+
+        response = {
+            received: req.query.input,
+            result: req.query.input*2
+        }
+    } else {
+        response = {
+            error: "Please provide input",
+        }
+    }
+        res.send(response);
+})
+app.listen(PORT, () => {
+    console.log(`I am listening on port ${PORT}`);
+})
