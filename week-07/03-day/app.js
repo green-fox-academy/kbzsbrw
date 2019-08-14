55f3176b5ed0f8fd6d502a85ce4aeb24bef7d46b
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-08-14 12:55:58 +0200

    format

--- week-07/03-day/app.js
+++ week-07/03-day/app.js
@@ -1,29 +1,28 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const app=express();
+const express = require("express");
+const app = express();
 const PORT = 8080;
-const path = require('path');
+const path = require("path");
 app.use(express.static("assets"));
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get('/doubling', (req,res) => {
-    let response = {};
-    if(req.query.input!==undefined){
-
-        response = {
-            received: req.query.input,
-            result: req.query.input*2
-        }
-    } else {
-        response = {
-            error: "Please provide input",
-        }
-    }
-        res.send(response);
-})
+app.get("/doubling", (req, res) => {
+  let response = {};
+  if (req.query.input !== undefined) {
+    response = {
+      received: req.query.input,
+      result: req.query.input * 2
+    };
+  } else {
+    response = {
+      error: "Please provide input"
+    };
+  }
+  res.send(response);
+});
 app.listen(PORT, () => {
-    console.log(`I am listening on port ${PORT}`);
-})
+  console.log(`I am listening on port ${PORT}`);
+});
