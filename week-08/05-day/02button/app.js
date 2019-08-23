a6660883caf5620a0904458cacc68be454bb8c04
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-08-23 17:01:38 +0200

    skeleton ready, statements not

--- week-08/05-day/02button/app.js
+++ week-08/05-day/02button/app.js
@@ -3,9 +3,12 @@
 const pets = document.querySelector('form[action="/pet"]');
 const dog = document.querySelector('#dog');
 const cat = document.querySelector('#cat');
+const fish = document.querySelector('#fish');
 const signUpButton = document.querySelector("button[type='submit']");
-const catFact = document.querySelector('#yes');
+const catFactYes = document.querySelector('#yes');
+const catFactNo = document.querySelector('#no');
 const loveCatsButton = document.querySelector("button[type='button']");
+const buttons = document.querySelectorAll('.button');
 
 pets.addEventListener('change', e => {
   if (cat.checked || dog.checked) {
@@ -14,5 +17,28 @@ pets.addEventListener('change', e => {
     signUpButton.disabled = true;
   }
 });
+catFactYes.addEventListener('change', e => {
+  if (catFactYes.checked) {
+    loveCatsButton.disabled = false;
+  } else {
+    loveCatsButton.disabled = true;
+  }
+});
 
-catFact.addEventListener('change', e => (loveCatsButton.disabled = false));
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    if (loveCatsButton.disabled) {
+      alert('Sigh, we still added you to the cat facts list');
+    } else {
+      alert("Thank you, you've successfully signed up for cat facts");
+    }
+  });
+});
+
+document.body.addEventListener('click', e => {
+  if (fish.checked && catFactNo.checked) {
+    signUpButton.disabled = false;
+  } else {
+    signUpButton.disabled = true;
+  }
+});
