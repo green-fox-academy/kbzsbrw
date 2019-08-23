e266cf3b16d11012559ebfdb3ceb5147af6d3ec4
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-08-23 15:22:20 +0200

    sign up button work(if dog or cat unchecked)

--- week-08/05-day/02button/app.js
+++ week-08/05-day/02button/app.js
@@ -1,10 +1,18 @@
 'use strict';
 
-const realPets = document.querySelectorAll('.real-pet');
+const pets = document.querySelector('form[action="/pet"]');
+const dog = document.querySelector('#dog');
+const cat = document.querySelector('#cat');
 const signUpButton = document.querySelector("button[type='submit']");
+const catFact = document.querySelector('#yes');
+const loveCatsButton = document.querySelector("button[type='button']");
 
-realPets.forEach(radio => {
-  radio.addEventListener('change', e => {
+pets.addEventListener('change', e => {
+  if (cat.checked || dog.checked) {
     signUpButton.disabled = false;
-  });
+  } else {
+    signUpButton.disabled = true;
+  }
 });
+
+catFact.addEventListener('change', e => (loveCatsButton.disabled = false));
