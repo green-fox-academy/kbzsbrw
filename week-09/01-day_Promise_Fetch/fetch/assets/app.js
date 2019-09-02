8d7ad4773d8b601c36aeaba6cd00ad2206d9c1ce
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-09-02 13:59:52 +0200

    done with async function and trycatch as well

--- week-09/01-day_Promise_Fetch/fetch/assets/app.js
+++ week-09/01-day_Promise_Fetch/fetch/assets/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const button = document.querySelector('button');
+const buttonAsync = document.querySelector('.async');
 const div = document.querySelector('div');
+const url = 'http://api.icndb.com/jokes/randomsi'
+
 function fetchJoke() {
   let p = document.createElement('p');
-  fetch('http://api.icndb.com/jokes/random')
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       p.innerText = data.value.joke;
@@ -12,7 +15,22 @@ function fetchJoke() {
     .catch(error => console.log(error));
   div.appendChild(p);
 }
+async function fetchJokeAsync() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    let p = document.createElement('p');
+    p.innerText = data.value.joke;
+    div.appendChild(p);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
 button.addEventListener('click', () => {
   event.preventDefault();
   fetchJoke();
+});
+buttonAsync.addEventListener('click', () => {
+  event.preventDefault();
+  fetchJokeAsync();
 });
