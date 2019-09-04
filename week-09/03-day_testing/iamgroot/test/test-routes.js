95f708c3475aafd54e80e5c37445101d45212928
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-09-04 16:49:49 +0200

    test for no msg

--- week-09/03-day_testing/iamgroot/test/test-routes.js
+++ week-09/03-day_testing/iamgroot/test/test-routes.js
@@ -4,17 +4,31 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
 
-test('groot endpont', (t) => {
+test('groot endpont with msg', t => {
   request(app)
     .get('/groot?message=anyadat')
     .send()
     .end((err, resp) => {
-      if(err) throw err;
+      if (err) throw err;
       t.same(resp.body, {
         received: 'anyadat',
         translated: 'I am groot'
       });
       t.equal(resp.status, 200);
+      t.end();
+    });
+});
+
+test('groot endpont without msg', t => {
+  request(app)
+    .get('/groot?')
+    .send()
+    .end((err, resp) => {
+      if (err) throw err;
+      t.same(resp.body, {
+        error: 'no msg bro'
+      });
+      t.equal(resp.status, 400);
       t.end();
     });
 });
