26de97671bc3cefa7cbcc4922a256ba1f4b56f39
[Kosztolanyi] <kosztolanyi.baji.zsolt@gmail.com>
2019-09-04 16:37:29 +0200

    no msg error handling

--- week-09/03-day_testing/iamgroot/routes.js
+++ week-09/03-day_testing/iamgroot/routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/groot', (req, res) => {
-  let message = req.query.message;
-  res.send({
-    received: message,
-    translated: 'I am groot'
-  });
+  if (req.query.message) {
+    let message = req.query.message;
+    res.send({
+      received: message,
+      translated: 'I am groot'
+    });
+  } else {
+    res.send({ error: 'no msg bro' });
+  }
 });
 
 module.exports = app;
